dd550614b1e0fb8ead0a4cfc7dad415a5f10c854
AIGQx <jalegalarza@gmail.com>
2025-06-11 04:21:51 -0500

    FIRST COMMIT

---
+++ url-shortener-frontend/src/main.ts
@@ -0,0 +1,51 @@
+let sendUrl = document.getElementById("urlShort") as HTMLButtonElement;
+let urlInput = document.getElementById("userUrl") as HTMLTextAreaElement;
+
+import Toastify from 'toastify-js'
+import "toastify-js/src/toastify.css"
+
+function shortUrl() {
+ //validate its an url
+  var urlText = urlInput.value as string;
+  if(urlText){
+    try {
+      new URL(urlText)
+      //continue
+      Toastify({
+        text: "generating your link...",
+        gravity: "bottom",
+        position:"center",
+        className: "toastAlert",
+        style: {
+          borderRadius: "12px",
+          background: "green"
+        }
+      }).showToast();
+
+    } catch (error) {
+      //catch the error
+      Toastify({
+        text: "not a valir URL",
+        gravity: "bottom",
+        position:"center",
+        className: "toastAlert",
+        style: {
+          borderRadius: "12px",
+          background: "red"
+        }
+      }).showToast();
+    }
+  }
+}
+
+urlInput.addEventListener('keyup', () => {
+  if(urlInput.value === ""){
+     sendUrl.disabled=true;
+  }
+  else{
+    sendUrl.disabled=false;
+  }
+})
+sendUrl.addEventListener('click', () => {
+  shortUrl();
+})
